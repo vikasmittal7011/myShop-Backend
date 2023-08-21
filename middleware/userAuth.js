3564d9f9dc997f5dea72be898535b6f7a94cd706
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-21 21:03:56 +0530

    Add admin & user auth middlware

---
+++ middleware/userAuth.js
@@ -0,0 +1,23 @@
+const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentication Failed!!");
+    }
+    const tokenValue = jwt.verify(token, process.env.JWT_TOKEN);
+    if (tokenValue.role === "user") {
+      req.userData = { userId: tokenValue.userid };
+      next();
+    } else {
+      return next(new HttpError("You are not right user!!", 401));
+    }
+  } catch (err) {
+    return next(new HttpError("Authentication Failed!!", 401));
+  }
+};
