9bd7ab5eec69d6b5de7fe525861e46047b587d0f
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-26 20:23:35 +0530

    Add auth to catgeory and brand

--- routes/BrandRoute.js
+++ routes/BrandRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { createBrandValiation } = require("../validation/Brand");
 const { fetchAllBrands, createBrand } = require("../controller/Brand");
+const adminAuth = require("../middleware/adminAuth");
 
 const router = express.Router();
 
-router.get("/", fetchAllBrands).post("/", createBrandValiation, createBrand);
+router
+  .get("/", fetchAllBrands)
+  .post("/", adminAuth, createBrandValiation, createBrand);
 
 module.exports = router;
--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -4,11 +4,12 @@ const {
   fetchAllCategories,
   createCategory,
 } = require("../controller/Category");
+const adminAuth = require("../middleware/adminAuth");
 
 const router = express.Router();
 
 router
   .get("/", fetchAllCategories)
-  .post("/", createCategoryValiation, createCategory);
+  .post("/", adminAuth, createCategoryValiation, createCategory);
 
 module.exports = router;
