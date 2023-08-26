45a0447cc55a077a87766ee4d751412890354b71
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-26 13:18:28 +0530

    Add general auth route

---
+++ middleware/generalAuth.js
@@ -0,0 +1,20 @@
+const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentication Failed!!");
+    }
+    const tokenValue = jwt.verify(token, process.env.JWT_TOKEN);
+    if (tokenValue) {
+      next();
+    }
+  } catch (err) {
+    return next(new HttpError("Authentication Failed!!", 401));
+  }
+};
