e23d0d1baadbc59cecb124666f4f9237c24092e4
vikas gupta <vikasaggrawal700@gmail.com>
2023-09-19 18:19:22 +0530

    Update build folder

---
+++ build/static/js/959.8f6e217a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[959],{7959:function(n,e,f){f.r(e);var t=f(2791),u=f(9434),c=f(2963),s=f(7689),o=f(2665),r=f(2981),a=f(2234),h=f(6639),k=f(4348),i=f(1397),b=f(2086);e.default=function(){var n=(0,u.I0)(),e=(0,s.s0)(),f=(0,u.v9)(c.s6).token;(0,t.useEffect)((function(){n((0,c.hh)()),n((0,o.FS)()),n((0,r.TF)()),n((0,a.Jq)()),n((0,h.N2)()),n((0,k.fc)()),n((0,i.xx)()),n((0,b.bx)()),e("/")}),[f,n,e])}}}]);
+//# sourceMappingURL=959.8f6e217a.chunk.js.map
